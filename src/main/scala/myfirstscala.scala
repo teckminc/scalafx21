a5a864bd10738528ed6cd7e266036d1cef91cdc6
teckminc <102782040+teckminc@users.noreply.github.com>
2024-10-02 22:38:04 +0800

    first commit

---
+++ src/main/scala/myfirstscala.scala
@@ -0,0 +1,10 @@
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+
+object MyApp extends JFXApp3:
+
+  override def start(): Unit =
+    stage = new PrimaryStage()
+
+
+end MyApp
